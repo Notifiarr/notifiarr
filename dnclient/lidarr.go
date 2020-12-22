0a5c3eda63237c7a65d510c66368975e9d71d7c4
David Newhall II <david@sleepers.pro>
2020-12-21 18:29:42 -0800

    lidar updates

--- dnclient/lidarr.go
+++ dnclient/lidarr.go
@@ -1,8 +1,12 @@
 package dnclient
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"golift.io/starr/lidarr"
 )
 
 /*
@@ -12,7 +16,7 @@ mbid - music brainz is the source for lidarr (todo)
 // lidarrHandlers is called once on startup to register the web API paths.
 func (c *Client) lidarrHandlers() {
 	c.handleAPIpath(Lidarr, "/add/{id:[0-9]+}", c.lidarrAddAlbum, "POST")
-	c.handleAPIpath(Lidarr, "/check/{id:[0-9]+}/{albumid:[0-9]+}", c.lidarrCheckAlbum, "GET")
+	c.handleAPIpath(Lidarr, "/check/{id:[0-9]+}/{albumid:[-a-z]+}", c.lidarrCheckAlbum, "GET")
 	c.handleAPIpath(Lidarr, "/qualityProfiles/{id:[0-9]+}", c.lidarrProfiles, "GET")
 	c.handleAPIpath(Lidarr, "/qualityDefinitions/{id:[0-9]+}", c.lidarrQualityDefs, "GET")
 	c.handleAPIpath(Lidarr, "/rootFolder/{id:[0-9]+}", c.lidarrRootFolders, "GET")
@@ -67,9 +71,40 @@ func (c *Client) lidarrQualityDefs(r *http.Request) (int, interface{}) {
 }
 
 func (c *Client) lidarrCheckAlbum(r *http.Request) (int, interface{}) {
-	return http.StatusLocked, "lidarr does not work yet"
+	// Check for existing movie.
+	if m, err := getLidarr(r).GetAlbum(mux.Vars(r)["albumid"]); err != nil {
+		return http.StatusServiceUnavailable, fmt.Errorf("checking album: %w", err)
+	} else if len(m) > 0 {
+		return http.StatusConflict, fmt.Errorf("%s: %w", mux.Vars(r)["albumid"], ErrExists)
+	}
+
+	return http.StatusOK, http.StatusText(http.StatusNotFound)
 }
 
 func (c *Client) lidarrAddAlbum(r *http.Request) (int, interface{}) {
-	return http.StatusLocked, "lidarr does not work yet"
+	var payload lidarr.AddAlbumInput
+	// Extract payload and check for TMDB ID.
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		return http.StatusBadRequest, fmt.Errorf("decoding payload: %w", err)
+	} else if payload == nil {
+		return http.StatusUnprocessableEntity, fmt.Errorf("0: %w", ErrNoGRID)
+	}
+
+	lidar := getLidarr(r)
+	// Check for existing album.
+	/* broken:
+	if m, err := lidar.GetAlbum(payload.AlbumID); err != nil {
+		return http.StatusServiceUnavailable, fmt.Errorf("checking album: %w", err)
+	} else if len(m) > 0 {
+		return http.StatusConflict, fmt.Errorf("%d: %w", payload.AlbumID, ErrExists)
+	}
+	*/
+
+	// Add book using payload.
+	book, err := lidar.AddAlbum(&payload)
+	if err != nil {
+		return http.StatusInternalServerError, fmt.Errorf("adding album: %w", err)
+	}
+
+	return http.StatusCreated, book
 }
