9931dc616d4c665aeec3d5ff3c58d1e27fd52c29
David Newhall II <david@sleepers.pro>
2021-11-21 21:24:24 -0800

    protect git commands, fix arrays

--- init/buildinfo.sh
+++ init/buildinfo.sh
@@ -4,20 +4,21 @@
 ##########
 
 VENDOR="Go Lift <code@golift.io>"
-
-# Dynamic. Recommend not changing.
-VVERSION=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1) 2>/dev/null)
-VERSION="$(echo $VVERSION | tr -d v | grep -E '^\S+$' || echo development)"
-# This produces a 0 in some envirnoments (like Homebrew), but it's only used for packages.
-ITERATION=$(git rev-list --count --all || echo 0)
 DATE="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
-COMMIT="$(git rev-parse --short HEAD || echo 0)"
-
-GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD || echo unknown)"
-BRANCH="${TRAVIS_BRANCH:-${GIT_BRANCH}}"
-
 # Defines docker manifest/build types.
 BUILDS="linux:armhf:arm linux:arm64:arm64 linux:amd64:amd64 linux:i386:386"
+
+if [ -x "$(which git)"]; then
+  # Dynamic. Recommend not changing.
+  VVERSION=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1) 2>/dev/null)
+  VERSION="$(echo $VVERSION | tr -d v | grep -E '^\S+$' || echo development)"
+  # This produces a 0 in some envirnoments (like Homebrew), but it's only used for packages.
+  ITERATION=$(git rev-list --count --all || echo 0)
+  COMMIT="$(git rev-parse --short HEAD || echo 0)"
+
+  GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD || echo unknown)"
+  BRANCH="${TRAVIS_BRANCH:-${GIT_BRANCH}}"
+fi
 
 export VENDOR VVERSION VERSION ITERATION DATE BRANCH COMMIT BUILDS
 
