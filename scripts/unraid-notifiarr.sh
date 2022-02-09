313951aa7325bc2eeb444a4c53917d30f571c196
David Newhall II <david@sleepers.pro>
2022-02-09 02:29:05 -0800

    cook script a bit more

--- scripts/unraid-notifiarr.sh
+++ scripts/unraid-notifiarr.sh
@@ -2,7 +2,7 @@ NOTIFIARR_API_KEY=""
 CHANNEL_ID="940876909158490154"
 
 ############
-# Use this script on your unRaid server to send notifications to notifiarr.com.
+# Use this script on your Unraid server to send notifications to Discord through notifiarr.com.
 ############
 # Setup Unraid:
 # * Create the agent directory if needed: mkdir -p /boot/config/plugins/dynamix/notifications/agents/
@@ -18,7 +18,21 @@ CHANNEL_ID="940876909158490154"
 # Inspiration: https://gist.github.com/ljm42/fa80ad36c20e9ae0b158b714865f67e2
 #
 
-##### SCRIPT BELOW #####
+# greyish, default left sidebar color.
+COLOR="28282C"
+# orangish, any importance != "normal" alert.
+ABNORMAL_COLOR="FF5733"
+
+##### SCRIPT BELOW, CHANGE ONLY IF YOU WANT TO BREAK THINGS #####
+
+# This stuff may work on OSes besides Unraid, but don't count on it.
+#UPTIME_SEC=$(head -n1 /proc/uptime | cut -d. -f1)
+UNRAIDVER=$(head -n1 /etc/issue | grep -o '[0-9]\+\.[0-9]\+\(\.[0-9]\+\)\?$')
+UPTIME=$(uptime)
+LOAD=$(echo "${UPTIME}" | grep -o '[0-9]\+\.[0-9]\+, [0-9]\+\.[0-9]\+, [0-9]\+\.[0-9]\+$')
+UPTIME=$(echo "${UPTIME}" | grep -o 'up[^,]\+')
+CONTAINERS=$(docker ps -q 2>&1 | wc -l)
+KERNEL=$(uname -s -r)
 
 # Available fields from notification system
 # HOSTNAME
@@ -29,56 +43,42 @@ CHANNEL_ID="940876909158490154"
 # CONTENT (notify -m)
 # TIMESTAMP (seconds from epoch)
 
-UNRAIDVER=$(head -n1 /etc/issue | grep -o '[0-9]\+\.[0-9]\+\(\.[0-9]\+\)\?$')
-#UPTIME_SEC=$(head -n1 /proc/uptime | cut -d. -f1)
-UPTIME=$(uptime | grep -o 'up[^,]\+')
-LOAD=$(uptime | grep -o '[0-9]\+\.[0-9]\+, [0-9]\+\.[0-9]\+, [0-9]\+\.[0-9]\+$')
-CONTAINERS=$(docker ps | wc -l)
-
-# for quick test, setup environment to mimic notify script
+# These values are generally only used when you run the script as a test.
 [[ "${EVENT}" ]]       || EVENT='No event'
 [[ "${SUBJECT}" ]]     || SUBJECT='No subject'
 [[ "${DESCRIPTION}" ]] || DESCRIPTION='No description'
-[[ "${IMPORTANCE}" ]]  || IMPORTANCE='No importance'
-[[ "${TIMESTAMP}" ]]   || TIMESTAMP=$(date +%s)
+[[ "${IMPORTANCE}" ]]  || IMPORTANCE='abnormal'
 [[ ! "${CONTENT}" ]]   || CONTENT=$(cat <<EOF
-, {"title": "Content", "text": "${CONTENT}"}
+,
+{"title": "Content", "text": "${CONTENT}"}
 EOF
 )
 
-COLOR="000000"
-[ "${IMPORTANCE}" == "normal" ] || COLOR="FF5733"
+# Change the color based on importance.
+[ "${IMPORTANCE}" == "normal" ] || COLOR="${ABNORMAL_COLOR}"
 
 PASSTHRU=$(cat <<EOF
 {
-    "notification": {
-        "update": false,
-        "name": "${HOSTNAME}",
-        "event": ""
-    },
-    "discord": {
-        "color": "${COLOR}",
-        "images": {
-            "thumbnail": "https://raw.githubusercontent.com/limetech/Unraid.net/master/Unraid.net.png"
-        },
-        "text": {
-            "title": "${EVENT}: ${SUBJECT}",
-            "content": "unRAID Alert! ${IMPORTANCE}: ${EVENT}",
-            "footer": "unRAID v${UNRAIDVER}",
-            "fields": [
-              {"title": "Description", "text": "${DESCRIPTION}"}${CONTENT},
-              {"title": "Load Avg", "text": "${LOAD}", "inline": true},
-              {"title": "Uptime", "text": "${UPTIME}", "inline": true},
-              {"title": "Dockers", "text": "${CONTAINERS}", "inline": true}
-            ]
-        },
-        "ids": {
-            "channel": ${CHANNEL_ID}
-        }
+  "notification": { "update": false, "name": "${EVENT}" },
+  "discord": {
+    "ids": { "channel": ${CHANNEL_ID} },
+    "color": "${COLOR}",
+    "text": {
+        "icon": "https://raw.githubusercontent.com/limetech/Unraid.net/master/Unraid.net.png",
+       "title": "${SUBJECT}",
+     "content": "${IMPORTANCE^} Unraid Alert! ${EVENT}",
+      "footer": "${HOSTNAME} • ${KERNEL} v${UNRAIDVER}",
+      "fields": [
+        {"title": "Description", "text": "${DESCRIPTION}"}${CONTENT},
+        {"title": "Load Avg",    "text": "${LOAD}",       "inline": true},
+        {"title": "Uptime",      "text": "${UPTIME}",     "inline": true},
+        {"title": "Dockers",     "text": "${CONTAINERS}", "inline": true}
+      ]
     }
+  }
 }
 EOF
 )
 
 curl -sH "x-api-key: ${NOTIFIARR_API_KEY}" -d "${PASSTHRU}" \
-        "https://dev.notifiarr.com/api/v1/notification/passthrough?event=unraid" 2>&1 > /dev/null
+        "https://notifiarr.com/api/v1/notification/passthrough?event=unraid" 2>&1 > /dev/null
