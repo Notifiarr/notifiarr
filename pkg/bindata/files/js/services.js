7711673ecbb700ac4d10e6f6f7934422d847ae13
David Newhall II <david@sleepers.pro>
2022-02-13 19:40:26 -0800

    fix services bug

--- pkg/bindata/files/js/services.js
+++ pkg/bindata/files/js/services.js
@@ -26,21 +26,21 @@ function addServiceCheck()
     const row = '<tr class="services-Checks" id="services-Checks-'+ index +'">'+
         '<td style="font-size: 22px;">'+ instance +
         '<div class="services-Checks-deletebutton" style="float: right;">'+
-        '<button onclick="removeInstance(\'services\', \'Checks\', '+ index +')" type="button" title="Delete this Service Check" class="delete-item-button btn btn-danger btn-sm"><i class="fa fa-minus"></i></button>'+
+        '<button onclick="removeInstance(\'services-Checks\', '+ index +')" type="button" title="Delete this Service Check" class="delete-item-button btn btn-danger btn-sm"><i class="fa fa-minus"></i></button>'+
         '</div>'+
         '</td>'+
         '<td><input type="text" id="Service.'+ index +'.Name" class="client-parameter" data-group="services" data-label="Check '+ instance +' Name" data-original="" value="Service'+ instance +'" style="width: 100%;"></td>'+
         '<td>'+
-        '<select id="Service.'+ index +'.Type" data-group="services" data-label="Check '+ instance +' Type" value="http" data-original="" style="width: 100%;">'+
+        '<select id="Service.'+ index +'.Type" class="client-parameter" data-group="services" data-label="Check '+ instance +' Type" value="http" data-original="" style="width: 100%;">'+
             '<option value="process">Process</option>'+
             '<option value="http">HTTP</option>'+
             '<option value="tcp">TCP Port</option>'+
         '</select>'+
         '</td>'+
-        '<td><input type="text" id="Service.'+ index +'.Value" data-group="services" data-label="Check '+ instance +' Value" data-original="" style="width: 100%;"></td>'+
-        '<td><input type="text" id="Service.'+ index +'.Expect" data-group="services" data-label="Check '+ instance +' Expect" data-original="" style="width: 100%;"></td>'+
-        '<td><input type="text" id="Service.'+ index +'.Interval" data-group="services" data-label="Check '+ instance +' Interval" data-original="5m" value="5m" style="width: 100%;"></td>'+
-        '<td><input type="text" id="Service.'+ index +'.Timeout" data-group="services" data-label="Check '+ instance +' Timeout" data-original="1m" value="1m" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Value" class="client-parameter" data-group="services" data-label="Check '+ instance +' Value" data-original="" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Expect" class="client-parameter" data-group="services" data-label="Check '+ instance +' Expect" data-original="" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Interval" class="client-parameter" data-group="services" data-label="Check '+ instance +' Interval" data-original="5m" value="5m" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Timeout" class="client-parameter" data-group="services" data-label="Check '+ instance +' Timeout" data-original="1m" value="1m" style="width: 100%;"></td>'+
         '</tr>';
 
     $('#services-Checks-container').append(row);
