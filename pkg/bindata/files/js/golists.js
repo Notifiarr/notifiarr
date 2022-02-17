75cdad9863bdcfed7f19475096fc497b2fdd383c
David Newhall II <david@sleepers.pro>
2022-02-17 01:42:23 -0800

    update reindexList and apply it to all lists

--- pkg/bindata/files/js/golists.js
+++ pkg/bindata/files/js/golists.js
@@ -14,10 +14,49 @@ function removeInstance(name, index)
     $('.'+ name + index +'-deleted').val(true);
 
     //-- FIX THE INDEXING
-    reindexList(name);
+    reindexList(name.split('-')[0]);
 
     // bring up the save changes button.
     findPendingChanges();
+}
+
+// The Go app only accepts indexes on lists starting at 0 with no gaps.
+// It looks better on screen to see inrimental numbers.
+// This procedure fixes the numbering on each row when an item is deleted.
+function reindexList(group)
+{
+    let counter = 0;
+    let currentIndex = 0;
+    let currentApp = '';
+
+    // Loop each form element with this data group and with a non-empty id.
+    $('[data-group='+ group+'][id]').each(function(index) {
+        const rowIndex = $(this).data("index");
+        const itemApp = $(this).data("app");
+
+        if (itemApp !== currentApp) {
+            // We get into here when the app (in the group) changes, and on the very first element.
+            counter = 0;
+            currentIndex = 0;
+        } else if (rowIndex !== currentIndex) {
+            // We get into here on any new element in the same app.
+            counter++;
+        }
+
+        //-- CHANGE id ATTR ON LABEL
+        $('#'+ itemApp +'IndexLabel'+ rowIndex).attr('id', itemApp +'IndexLabel'+ counter).html(counter + 1);
+
+        //-- CHANGE data-label ON INPUT FIELDS
+        $(this).data('label', $(this).data('label').replace((rowIndex + 1), (counter + 1)));
+
+        //-- CHANGE id ATTR ON INPUT FIELDS
+        $(this).attr('id', $(this).attr('id').replace(rowIndex, counter));
+
+        //-- SET CURRENT VARIABLES
+        $(this).data("index", counter)
+        currentIndex = rowIndex;
+        currentApp = itemApp;
+    });
 }
 
 // addInstance is used to build the table rows.
@@ -30,7 +69,7 @@ function addInstance(section, app)
     const names = $('#'+ section +'-'+ app +'-addbutton').data('names'); // list of thinges like "Name" and "URL"
     // This just sets the first few lines of the row. The more-dynamic bits get added below, in a for loop.
     let row = '<tr class="'+ section +'-'+ app +'" id="'+ section +'-'+ app +'-'+ instance +'">'+
-                '   <td style="font-size: 22px;"><span id="'+ lowercaseWord(app) +'-index-label-'+ (instance-1) +'">'+instance+"</span>"+
+                '   <td style="font-size: 22px;"><span id="'+ app +'IndexLabel'+ (instance-1) +'">'+instance+"</span>"+
                 '       <div class="'+ section +'-'+ app +'-deletebutton" style="float: right;">'+
                 '           <button onclick="removeInstance(\''+ section +'-'+ app +'\', '+ instance +
                             ')" type="button" title="Delete this instance of '+ app +'.'+
@@ -70,7 +109,7 @@ function addInstance(section, app)
         }
 
         row +=  '<td colspan="'+ colspan +'"><input type="text" id="'+ prefix +'.'+ app +'.'+ index +'.'+ name +
-        '" class="client-parameter" data-group="'+ section +'" data-label="'+ app +' '+
+        '" data-app="'+ app +'" data-index="'+ index +'" class="client-parameter" data-group="'+ section +'" data-label="'+ app +' '+
          instance +' '+name+'" data-original="'+ nameori +'" value="'+ nameval +'" style="width: 100%;"></td>';
          colspan = 1;
     }
--- pkg/bindata/files/js/services.js
+++ pkg/bindata/files/js/services.js
@@ -33,33 +33,33 @@ function addServiceCheck()
     const index = $('.services-Checks').length;
     const instance = index+1;
     const row = '<tr class="services-Checks" id="services-Checks-'+ index +'">'+
-        '<td style="font-size: 22px;"><span id="service-index-label-'+ index +'">'+ instance +'</span>'+
+        '<td style="font-size: 22px;"><span id="checksIndexLabel'+ index +'">'+ instance +'</span>'+
         '<div class="services-Checks-deletebutton" style="float: right;">'+
         '<button onclick="removeInstance(\'services-Checks\', '+ index +')" type="button" title="Delete this Service Check" class="delete-item-button btn btn-danger btn-sm"><i class="fa fa-minus"></i></button>'+
         '</div>'+
         '</td>'+
-        '<td><input type="text" id="Service.'+ index +'.Name" class="client-parameter" data-group="services" data-label="Check '+ instance +' Name" data-original="" value="Service'+ instance +'" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Name" data-app="checks" data-index="'+ index +'" class="client-parameter" data-group="services" data-label="Check '+ instance +' Name" data-original="" value="Service'+ instance +'" style="width: 100%;"></td>'+
         '<td>'+
-        '<select id="Service.'+ index +'.Type" class="client-parameter" data-group="services" data-label="Check '+ instance +' Type" value="http" data-original="" style="width: 100%;">'+
+        '<select id="Service.'+ index +'.Type" data-app="checks" data-index="'+ index +'" class="client-parameter" data-group="services" data-label="Check '+ instance +' Type" value="http" data-original="" style="width: 100%;">'+
             '<option value="process">Process</option>'+
             '<option value="http">HTTP</option>'+
             '<option value="tcp">TCP Port</option>'+
         '</select>'+
         '</td>'+
-        '<td><input type="text" id="Service.'+ index +'.Value" class="client-parameter" data-group="services" data-label="Check '+ instance +' Value" data-original="" style="width: 100%;"></td>'+
-        '<td><input type="text" id="Service.'+ index +'.Expect" class="client-parameter" data-group="services" data-label="Check '+ instance +' Expect" data-original="" style="width: 100%;"></td>'+
-        '<td><input type="text" id="Service.'+ index +'.Interval" class="client-parameter" data-group="services" data-label="Check '+ instance +' Interval" data-original="5m" value="5m" style="width: 100%;"></td>'+
-        '<td><input type="text" id="Service.'+ index +'.Timeout" class="client-parameter" data-group="services" data-label="Check '+ instance +' Timeout" data-original="1m" value="1m" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Value" data-app="checks" data-index="'+ index +'" class="client-parameter" data-group="services" data-label="Check '+ instance +' Value" data-original="" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Expect" data-app="checks" data-index="'+ index +'" class="client-parameter" data-group="services" data-label="Check '+ instance +' Expect" data-original="" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Interval" data-app="checks" data-index="'+ index +'" class="client-parameter" data-group="services" data-label="Check '+ instance +' Interval" data-original="5m" value="5m" style="width: 100%;"></td>'+
+        '<td><input type="text" id="Service.'+ index +'.Timeout" data-app="checks" data-index="'+ index +'" class="client-parameter" data-group="services" data-label="Check '+ instance +' Timeout" data-original="1m" value="1m" style="width: 100%;"></td>'+
         '</tr>';
 
     $('#services-Checks-container').append(row);
 
     // redo tooltips since some got added.
     setTooltips();
-    
+
     // hide the "no instances" item that displays if no instances are configured.
     $('#services-Checks-none').hide();
-    
+
     // bring up the save changes button.
     findPendingChanges();
 }
