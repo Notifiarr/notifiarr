00d8a08aafc20397351463ee7ddc5970cef1938b
David Newhall II <david@sleepers.pro>
2022-03-16 21:37:39 -0700

    move starr setup til after startup

--- pkg/client/start.go
+++ pkg/client/start.go
@@ -110,6 +110,12 @@ func (c *Client) start() error { //nolint:cyclop
 	}
 
 	c.Logger.SetupLogging(c.Config.LogConfig)
+
+	// Make sure each app has a sane timeout.
+	if err = c.Config.Apps.Setup(c.Config.Timeout.Duration); err != nil {
+		return fmt.Errorf("setting up app: %w", err)
+	}
+
 	c.Printf("%s v%s-%s Starting! [PID: %v] %v",
 		c.Flags.Name(), version.Version, version.Revision, os.Getpid(), time.Now())
 	c.Printf("==> %s", msg)
