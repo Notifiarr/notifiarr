104fcbe0031836945fc67765a3db18ad9fe78797
David Newhall II <david@sleepers.pro>
2022-12-21 02:45:20 -0800

    try something else

--- scripts/makedmg.sh
+++ scripts/makedmg.sh
@@ -15,14 +15,15 @@ if [ -n "$TRAVIS_OS_NAME" ]; then
   security create-keychain -p secret $KEYCHAIN
 
   echo "Importing certificate into ${KEYCHAIN}"
-  echo | openssl pkcs12 -in apple.signing.key -clcerts -nokeys -out certificate.cer -password stdin
-  security import certificate.cer -f pkcs12 -k $KEYCHAIN -T /usr/local/bin/gon
+  security import apple.signing.key -P '' -f pkcs12 -k $KEYCHAIN -T /usr/local/bin/gon
 
   echo "Unlocking keychain ${KEYCHAIN}"
   security unlock-keychain -p secret $KEYCHAIN
 
   echo "Increase keychain unlock timeout to 1 hour."
   security set-keychain-settings -lut 3600 $KEYCHAIN
+  
+  security set-key-partition-list -S apple-tool:,apple: -s -k secret $KEYCHAIN
 
   echo "Add keychain to keychain-list"
   security list-keychains -s $KEYCHAIN
@@ -31,68 +32,6 @@ fi
 echo "Signing App."
 gon init/macos/sign.json
 
-# Initial r/w image.
-echo "Creating pack.temp.dmg."
-rm -f pack.temp.dmg
-hdiutil create -srcfolder "${MACAPP}.app" -volname "${MACAPP}" -fs HFS+ \
-      -fsargs "-c c=64,a=16,e=16" -format UDRW -size 200000k pack.temp.dmg
-
-# Get device.
-hdiutil detach "/Volumes/${MACAPP}"
-sleep 1
-echo "Attaching pack.temp.dmg."
-device=$(hdiutil attach -readwrite -noverify -noautoopen "pack.temp.dmg" | \
-         egrep '^/dev/' | sed 1q | awk '{print $1}')
-
-# Create content.
-sleep 1
-echo "Copying background."
-mkdir "/Volumes/${MACAPP}/.background"
-cp -r init/macos/background.png "/Volumes/${MACAPP}/.background/${MACAPP}.png"
-
-echo "Running AppleScript to build custom DMG."
-echo '
-   tell application "Finder"
-     tell disk "'${MACAPP}'"
-           open
-           set current view of container window to icon view
-           set toolbar visible of container window to false
-           set statusbar visible of container window to false
-           set the bounds of container window to {400, 100, 1320, 600}
-           set theViewOptions to the icon view options of container window
-           set arrangement of theViewOptions to not arranged
-           set icon size of theViewOptions to 256
-           set background picture of theViewOptions to file ".background:'${MACAPP}.png'"
-           make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
-           set position of item "'${MACAPP}.app'" of container window to {0, 0}
-           set position of item "Applications" of container window to {600, 0}
-           update without registering applications
-           delay 1
-           close
-     end tell
-   end tell
-' | osascript
-
-sleep 1
-# Finalize.
-echo "Finalizing DMG."
-chmod -Rf go-w /Volumes/"${MACAPP}"
-
-sleep 1
-echo "Syncing."
-#sync
-#sync
-
-sleep 1
-echo "Detaching ${device}."
-hdiutil detach ${device}
-
-sleep 1
-echo "Converting DMG."
+# Creating non-notarized DMG.
 mkdir -p release
-rm -f "release/${MACAPP}.dmg"
-hdiutil convert "pack.temp.dmg" -format UDZO -imagekey zlib-level=9 -o "release/${MACAPP}.dmg"
-rm -f pack.temp.dmg 
-
-echo "Notarizing DMG."
-gon init/macos/notarize.json
+hdiutil create release/$(MACAPP)-unsigned.dmg -srcfolder $(MACAPP).app -ov
--- scripts/unstable-deploy.sh
+++ scripts/unstable-deploy.sh
@@ -6,8 +6,10 @@ if [ -z "${UNSTABLE_UPLOAD_KEY}" ]; then
 fi
 
 [ -f releases/Notifiarr.dmg ] &&
-    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" https://unstable.notifiarr.app/upload.php -F "file=@releases/Notifiarr.dmg"
+    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" https://unstable.notifiarr.app/upload.php -F "file=@release/Notifiarr.dmg"
+[ -f releases/Notifiarr-unsigned.dmg ] &&
+    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" https://unstable.notifiarr.app/upload.php -F "file=@release/Notifiarr-unsigned.dmg"
 [ -f releases/notifiarr.amd64.gz ] &&
-    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" https://unstable.notifiarr.app/upload.php -F "file=@releases/notifiarr.amd64.gz"
+    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" https://unstable.notifiarr.app/upload.php -F "file=@release/notifiarr.amd64.gz"
 [ -f releases/notifiarr.amd64.exe ] &&
-    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" https://unstable.notifiarr.app/upload.php -F "file=@releases/notifiarr.amd64.exe"
+    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" https://unstable.notifiarr.app/upload.php -F "file=@release/notifiarr.amd64.exe"
