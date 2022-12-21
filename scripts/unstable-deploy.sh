02dabb03382d79611cf85ee784df4402d8ab5dec
David Newhall II <david@sleepers.pro>
2022-12-21 04:19:21 -0800

    another try for upload

--- scripts/unstable-deploy.sh
+++ scripts/unstable-deploy.sh
@@ -5,12 +5,15 @@ source settings.sh
 URL="https://unstable.notifiarr.app/upload.php"
 
 if [ -z "${UNSTABLE_UPLOAD_KEY}" ]; then
-  echo "No upload key."
+  echo "No upload key for unstable."
   exit 0
 fi
 
+echo "Uploading unstable files."
+
 for file in release/*.{zip,dmg,gz,txz}; do
   if [ -f "$file" ]; then
+    echo "Uploading: $file"
     curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" "$URL" -F "file=@$file"
   fi
 done
