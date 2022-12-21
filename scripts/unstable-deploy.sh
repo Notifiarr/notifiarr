9897cbc5f73c432bebef639f0719bb0c5460e4f4
David Newhall II <david@sleepers.pro>
2022-12-21 14:01:08 -0800

    why are uploads broken

--- scripts/unstable-deploy.sh
+++ scripts/unstable-deploy.sh
@@ -9,11 +9,14 @@ if [ -z "${UNSTABLE_UPLOAD_KEY}" ]; then
   exit 0
 fi
 
+curl --version
+echo
 echo "Uploading unstable files."
 
 for file in release/*.{zip,dmg,gz,txz}; do
   if [ -f "$file" ]; then
     echo "Uploading: $file"
-    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" "$URL" -F "file=@$file"
+    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" "$URL" -XPOST -F "file=@$file;filename=$(basename $file)"
+    echo
   fi
 done
