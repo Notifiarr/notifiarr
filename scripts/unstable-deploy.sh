6184507e1fad8d6e65d20516772918deb2424c83
David Newhall II <david@sleepers.pro>
2022-12-22 03:18:22 -0800

    travis

--- scripts/unstable-deploy.sh
+++ scripts/unstable-deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source settings.sh
+
 URL="https://unstable.notifiarr.app/upload.php"
 
 if [ -z "${UNSTABLE_UPLOAD_KEY}" ]; then
@@ -9,10 +11,11 @@ fi
 
 echo "Uploading unstable files."
 
-for file in release/*.{zip,dmg,gz,txz}; do
+for file in release/*.{zip,dmg,gz}; do
   if [ -f "$file" ]; then
     echo "Uploading: $file"
-    curl -H "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" "$URL" -F "file=@$file"
-    echo
+    curl -sSH "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" "$URL" -F "file=@$file"
+    versionfile="$VERSION-$ITERATION;filename=$(basname $file).txt;type=text/plain"
+    curl -sSH "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" "$URL" -F "file=$versionfile"
   fi
 done
