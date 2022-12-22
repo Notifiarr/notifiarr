2567cbb632398faacb7e0b53238bff1706e00c6e
David Newhall II <david@sleepers.pro>
2022-12-22 03:33:08 -0800

    rip

--- scripts/unstable-deploy.sh
+++ scripts/unstable-deploy.sh
@@ -15,7 +15,7 @@ for file in release/*.{zip,dmg,gz}; do
   if [ -f "$file" ]; then
     echo "Uploading: $file"
     curl -sSH "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" "$URL" -F "file=@$file"
-    versionfile="$VERSION-$ITERATION;filename=$(basname $file).txt;type=text/plain"
+    versionfile="$VERSION-$ITERATION;filename=$(basename $file).txt;type=text/plain"
     curl -sSH "X-API-KEY: ${UNSTABLE_UPLOAD_KEY}" "$URL" -F "file=$versionfile"
   fi
 done
