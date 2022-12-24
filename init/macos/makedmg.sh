dfa274f75b5b39c171bfcb67e2b91769edfe5ced
David Newhall II <david@sleepers.pro>
2022-12-24 00:40:35 -0800

    trim more travis

--- init/macos/makedmg.sh
+++ init/macos/makedmg.sh
@@ -5,19 +5,16 @@
 
 set -e -o pipefail
 
-# If we are running in Travis, make a new keychain and import the certificate.
-if [ -n "$TRAVIS_OS_NAME" ] || [ -n "$APPLE_SIGNING_KEY" ]; then
+# If we are running in GH Actions, make a new keychain and import the certificate.
+if [ -n "$APPLE_SIGNING_KEY" ]; then
   KEYCHAIN="ios-build.keychain"
 
   echo "==> Creating new keychain: $KEYCHAIN"
   security create-keychain -p secret $KEYCHAIN
 
   echo "==> Importing certificate into ${KEYCHAIN}"
-  if [ -f "apple.signing.key" ]; then
-    security import apple.signing.key -P '' -f pkcs12 -k $KEYCHAIN -T /usr/bin/codesign
-  else
-    echo "${APPLE_SIGNING_KEY}" | base64 -d | security import /dev/stdin -P "${AC_PASSWORD}" -f pkcs12 -k $KEYCHAIN -T /usr/bin/codesign
-  fi
+  echo "${APPLE_SIGNING_KEY}" | base64 -d | \
+    security import /dev/stdin -P "${AC_PASSWORD}" -f pkcs12 -k $KEYCHAIN -T /usr/bin/codesign
 
   echo "==> Unlocking keychain ${KEYCHAIN}"
   security unlock-keychain -p secret $KEYCHAIN
