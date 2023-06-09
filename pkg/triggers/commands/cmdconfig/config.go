1a8de75cd8d4008a5c7ce09df26844bcb8d1741b
David Newhall II <david@sleepers.pro>
2023-06-08 21:46:05 -0700

    fix command hash again

--- pkg/triggers/commands/cmdconfig/config.go
+++ pkg/triggers/commands/cmdconfig/config.go
@@ -6,11 +6,11 @@ import "golift.io/cnfg"
 
 type Config struct {
 	Name    string        `json:"name" toml:"name" xml:"name" yaml:"name"`
+	Hash    string        `json:"hash" toml:"hash" xml:"hash" yaml:"hash"`
 	Command string        `json:"-" toml:"command" xml:"command" yaml:"command"`
 	Shell   bool          `json:"shell" toml:"shell" xml:"shell" yaml:"shell"`
 	Log     bool          `json:"log" toml:"log" xml:"log" yaml:"log"`
 	Notify  bool          `json:"notify" toml:"notify" xml:"notify" yaml:"notify"`
 	Timeout cnfg.Duration `json:"-" toml:"timeout" xml:"timeout" yaml:"timeout"`
-	Hash    string        `json:"hash" toml:"hash" xml:"hash" yaml:"hash"`
 	Args    int           `json:"args" toml:"-" xml:"-" yaml:"-"`
 }
