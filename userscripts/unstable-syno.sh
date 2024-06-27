e98ee270afeadb8fc2d626b4245db37cc1b26be3
David Newhall II <david@sleepers.pro>
2024-06-27 11:34:24 -0700

    fix typo

--- userscripts/unstable-syno.sh
+++ userscripts/unstable-syno.sh
@@ -11,7 +11,7 @@ set -e
 # This is where the log file goes, and where temporary downloads are stored.
 # It must exist, and be writable by the user running this script.
 WORKDIR="/volume1/homes/notifiarr/"
-# Unstable website URL. This redirects to glift.io/notifiarr/.
+# Unstable website URL. This redirects to golift.io/notifiarr/.
 UNSTABLE="https://unstable.notifiarr.app"
 # File to download/check for updates. Do not include the .gz or .txt suffixes.
 FILE="notifiarr.amd64.linux"
