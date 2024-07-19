62f9353ce9051d2d5b1f810a983151df33de0ea5
David Newhall II <david@sleepers.pro>
2024-07-18 18:13:51 -0700

    missed a file

---
+++ pkg/bindata/bindata.go
@@ -0,0 +1,13 @@
+// Package bindata provides file system assets to the running binary.
+package bindata
+
+import "embed"
+
+var (
+	//go:embed files
+	Files embed.FS
+	//go:embed templates
+	Templates embed.FS
+	//go:embed other/fortunes.txt
+	Fortunes string
+)
