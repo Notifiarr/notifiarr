68fc10585e410d937c8642094833016a81c2cd86
David Newhall II <david@sleepers.pro>
2025-06-15 14:33:21 -0700

    missing file

---
+++ frontend/src/navigation/goto-element.svelte
@@ -0,0 +1,10 @@
+<!-- Used in translations to link to a local page. -->
+<svelte:options customElement={{ tag: 'go-to', shadow: 'open' }} />
+
+<script>
+  import { urlbase } from '../api/fetch'
+  import { nav } from './nav.svelte'
+  let { page = 'landing' } = $props()
+</script>
+
+<a href={$urlbase + page} onclick={e => nav.goto(e, page)}><slot /></a>
